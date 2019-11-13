cbf9eaf47f0fd24f482edc46ac44e256cdc7c89a
Alexey <volkov.aa@phystech.edu>
2019-11-14 00:25:43 +0300

    Created project

---
+++ cmake-build-debug/CMakeFiles/Akinator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Akinator"
+  "Akinator.pdb"
+  "CMakeFiles/Akinator.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Akinator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
