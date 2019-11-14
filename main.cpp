9d46d4296ed8487b6c324b18e68d3275feb004ac
Alexey <volkov.aa@phystech.edu>
2019-11-14 23:35:21 +0300

    Implemented mode selection menu

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@
 #include <ncurses.h>
 #include "../Tree/Tree.h"
 
+enum GAMEMODES {
+    PLAY = 0,
+    COMPARE = 1,
+    PRINT = 2,
+    EXIT = 3
+};
+
 enum VOICES {
     OKSANA,
     JANE,
@@ -19,12 +26,15 @@ enum VOICES {
     ALENA,
     FILIPP
 };
+const int KEY_ACTUAL_ENTER = 10;
 
 size_t MAX_VOICE_SIZE = 10;
 size_t MAX_EMOTION_SIZE = 7;
 size_t MAX_DUMMY_SIZE = 30;
 
 size_t SUBQUESTION_SIZE = 65;
+
+size_t MAX_LINE_SIZE = 129;
 
 enum EMOTIONS {
     GOOD,
@@ -42,16 +52,34 @@ void say(const char *prompt, VOICES voice, EMOTIONS emotion);
 
 void akinatorPlayNode(node_t *node);
 
+int askMode();
+
 int main() {
-    setlocale (LC_ALL, "");
+    setlocale(LC_ALL, "");
     initscr();
     char *serializedDT = loadFile("decisionTreeSerialized.txt");
     tree_t *decisionTree = treeDeserialize(serializedDT, deserializePrompt);
-    // say("Дерево решений загружено.", FILIPP, NEUTRAL);
-    say("Привет! Сыграем в игру? Загадай персонажа, и я отгадаю его!", ALENA, NEUTRAL);
+    printw("Привет! Пожалуйста, выбери режим работы:\n");
+    say("Привет! Пожалуйста, выбери режим работы", ALENA, NEUTRAL);
 
-    akinatorPlayNode(decisionTree->head);
-    getch();
+    int mode = 0;
+    int y = 0;
+    int x = 0;
+    while ((mode = askMode()) != 3) {
+        switch (mode) {
+            case PLAY:
+                addch('\n');
+                akinatorPlayNode(decisionTree->head);
+                break;
+            default:
+                say("Простите, но эта фича не имплементирована", ALENA, NEUTRAL);
+                getyx(stdscr, y, x);
+                move(y, 1);
+                break;
+        }
+    }
+
+    say("Всего доброго!", ALENA, NEUTRAL);
     endwin();
     return 0;
 }
@@ -61,7 +89,53 @@ void *deserializePrompt(char *ser) {
 }
 
 int askMode() {
-    char modes[][20] = {"Играть", "Сравнить", "Вывести", "Выйти"};
+    char modes[][20] = {"Играть", "Сравнить", "Дерево", "Выйти"};
+    int modes_num = 4;
+
+    curs_set(0);
+    keypad(stdscr, true);
+    noecho();
+
+    int pos = 0;
+    int ch = 0;
+    int x = 0;
+    int y = 0;
+    getyx(stdscr, y, x);
+
+    while (ch != KEY_ACTUAL_ENTER) {
+        switch (ch) {
+            case KEY_RIGHT:
+                pos++;
+                break;
+            case KEY_LEFT:
+                pos--;
+                break;
+        }
+
+        if (pos == -1) pos = modes_num - 1;
+        else if (pos == modes_num) pos = 0;
+
+
+        wmove(stdscr, y, x);
+        for (int i = 0; i < modes_num; i++) {
+            if (i == pos)
+                attron(A_STANDOUT);
+
+            printw(modes[i]);
+
+            if (i == pos)
+                attroff(A_STANDOUT);
+
+            addch(' ');
+        }
+        refresh();
+
+        ch = wgetch(stdscr);
+    }
+
+    keypad(stdscr, false);
+    echo();
+    return pos;
 }
 
 int askYesNo() {
@@ -73,7 +147,7 @@ int askYesNo() {
     int x = 0;
     int y = 0;
     getyx(stdscr, y, x);
-    while(ch != 10) {
+    while (ch != KEY_ACTUAL_ENTER) {
         switch (ch) {
             case KEY_RIGHT:
                 pos += 1;
@@ -105,15 +179,14 @@ int askYesNo() {
 
     keypad(stdscr, false);
     echo();
-    nodelay(stdscr, false);
 
-    if(pos == 0)
+    if (pos == 0)
         mvprintw(y, x, "Ваш ответ: Да\n");
 
-    if(pos == 1)
+    if (pos == 1)
         mvprintw(y, x, "Ваш ответ: Нет\n");
     refresh();
-    return pos;
+    return !pos;
 }
 
 size_t getFilesize(FILE *f) {
@@ -147,7 +220,7 @@ void say(const char *prompt, VOICES voice, EMOTIONS emotion) {
     char *request = (char *) calloc(MAX_DUMMY_SIZE + MAX_EMOTION_SIZE + MAX_VOICE_SIZE + length, sizeof(char));
     size_t offset = sprintf(request, "./say.sh \"%s\" ", prompt);
 
-    switch(voice) {
+    switch (voice) {
         case OKSANA:
             offset += sprintf(request + offset, "%s ", "oksana");
             break;
@@ -183,7 +256,7 @@ void say(const char *prompt, VOICES voice, EMOTIONS emotion) {
             break;
     }
 
-    switch(emotion) {
+    switch (emotion) {
         case GOOD:
             sprintf(request + offset, "%s", "good 1&>2 /dev/null");
             break;
@@ -198,26 +271,39 @@ void say(const char *prompt, VOICES voice, EMOTIONS emotion) {
     free(request);
 }
 
+void addSplitRoutine(node_t *node) {
+    printw("Что вы задали?");
+    refresh();
+    printw("Я загадал: ");
+    getline()
+}
+
 void akinatorPlayNode(node_t *node) {
     assert(node);
-    if(!node->right && !node->left) {
+    int ans = -1;
+    if (!node->right && !node->left) {
         char *str = (char *) calloc(SUBQUESTION_SIZE + strlen((char *) node->value), sizeof(char));
         sprintf(str, "Вы загадали %s, не так ли?\n", (char *) node->value);
         printw("%s", str);
+        refresh();
+
         say(str, ALENA, NEUTRAL);
+        ans = askYesNo();
+        if (ans) {
+            printw("Так и знала! Что будем делать дальше?\n");
+            say("Так и знала! Что будем делать дальше?", ALENA, NEUTRAL);
+        }
+
     } else {
         char *str = (char *) calloc(SUBQUESTION_SIZE + strlen((char *) node->value), sizeof(char));
         sprintf(str, "%s?\n", (char *) node->value);
         printw("%s", str);
+        refresh();
         say(str, ALENA, NEUTRAL);
-        int ans = askYesNo();
-//        char answer[5] = "";
-//        while (strcmp(answer, "y") && strcmp(answer, "n")) {
-//            printw("Ответ: ");
-//            scanw("%s", answer);
-//        }
 
-        if (ans == 0)
+        ans = askYesNo();
+
+        if (ans)
             akinatorPlayNode(node->right);
         else
             akinatorPlayNode(node->left);
